d3faa29837a80d1e66cbfe692bdd59afba965bf0
MrCactus753 <MrCactus753@users.noreply.github.com>
2019-10-27 20:34:54 +0100

    general improvements

--- Classroom_Conversation_Measurer.py
+++ Classroom_Conversation_Measurer.py
@@ -4,8 +4,9 @@ import csv
 
 def csv_expander(list_of_lists):
     today = datetime.now()
-    date = today.strftime('%d_%m_%y_%H_%M')
-    filename = date +'.csv'
+    klasse = str(input('In which classroom was this hospitation? \n>>>'))
+    date = today.strftime('%d_%m_%y')
+    filename = date + '_'+ klasse+'.csv'
     with open(filename,'w',newline='') as protocol:
         filewriter = csv.writer(protocol, delimiter=',')
         filewriter.writerows(list_of_lists)
@@ -17,30 +18,23 @@ def timer(starttime):
     oldtime = starttime
     newtime = starttime
     entries = [['role', 'time spoken']]
-    speaker_roles = {
+    speaker_roles={
     's':'student',
-    't':'teacher',
-    'ts':'teacher_and_students',
-    'st': 'teacher_and_students',
-    'ss':'students_and_students',
-    'x':'nobody'}
+    'd':'teacher',
+    'a':'nobody'}
     print('Please enter one of the following roles: '
           '\n\"s\": STUDENT'
-          '\n\"t\": TEACHER'
-          '\n\"st\": STUDENT & TEACHER'
-          '\n\"ss\": STUDENT & STUDENT'
-          '\n\"x\": NO SPEAKER')
+          '\n\"d\": TEACHER'
+          '\n\"a\": NO SPEAKER')
 
     while True:
         command = str(input('>>> '))
         try:
-            '''if command == 'start':
-                start = datetime.now()
-                newtime = start
-                pass'''
             if command == 'q':
                 print('Thanks for using the program, please retrive your data in the working directory.')
                 total_time = datetime.now() - start
+                #focus = str(input('Was war der Fokus dieser Stunde? \n >>> '))
+                #klasse = str(input('In which classroom was this hospitation? \n>>>'))
                 entries.append(['total time',total_time.total_seconds()])
                 csv_expander(entries)
                 break
@@ -60,6 +54,7 @@ def start():
     i = str(input('To start a new timing process, type \"start\". \nIf you want to quit recording after starting, simply enter \"q\".\n>>> '))
     if i == 'start':
         first_time = datetime.now()
+        #Klasse = str(input('Great, which class are you observing?\n>>> '))
         timer(first_time)
     elif i == 'q':
         print('Goodbye!')
@@ -70,4 +65,3 @@ def start():
 
 
 start()
-
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # convo_measurer
-A short program that I made to track the relation between the talking parts of students and teachers during class. It's not perfect by far, but I found it to be quite useful. 
+A short program that I made to track the relation between the talking parts of students and teachers during class. It's not perfect by far, but I found it to be quite useful.
+
+# HOW TO USE
+1. Download the file with which you want to work (.py or .exe)
+2. Run the file on your computer
+3. To initialize the timer, type start into the main menue of the application
+  1. The timer starts as soon as the ENTER-key is pressed
+5. To mark the end of a speaking interval, simply enter the corrosponding letter for the person who's interval just ended and press the ENTER-key. (eg. if the teacher begins the lesson with a 15 min monologue, you can enter d, and the interval will be logged as soon as you press ENTER)
+6. Should you accidentally press a wrong key (anything other than a,s,d or q) the program will simply ignore it.
+7. To end the recording, simply enter q and confirm. The program will then ask you to give a classroom in which the hospitation took place.
+8. The program terminates automatically. Afterwards the gathered data will be saved in the same directory as the program.
+### IMPORTANT
+Right now the program does NOT feature a mechanism to revert faulty entries, so I would reccomend you to take care.
+
+
+
+# Version List:
+- 0.1:
+  - initial release of the program
+- 0.2 (27.10.19):
+  - added new file naming system
+  - added manual to readme-file
